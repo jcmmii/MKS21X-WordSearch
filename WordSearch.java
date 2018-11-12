dc8dcb13cbcd65dd8610ee815a274cf3e233b251
Justin Chen <jchen06@stuy.edu>
2018-11-12 01:32:10 -0500

    Working addAllWords? Needs testing

--- WordSearch.java
+++ WordSearch.java
@@ -36,7 +36,7 @@ public class WordSearch{
      }
 
    /**Set all values in the WordSearch to underscores'_'*/
-    public void clear() {
+    private void clear() {
       for (int x = 0; x < data.length; x++) {
         for (int y = 0; y < data[x].length; y++) {
           data[x][y] = '_';
@@ -106,10 +106,35 @@ public class WordSearch{
       return true;
     }
 
-    public void addAllWords() {
+    private void addAllWords() {
+      int[] dir = new int[3];
+      cols[0] = -1;
+      cols[1] = 0;
+      cols[2] = 1;
+      int Attempts = 200;
+      int Tries = 0;
+      while (Tries < Attempts || wordsToAdd.length > 0) {
+        int a = new Random().nextInt(3);
+        int b = new Random().nextInt(3);
+        int RDir = dir.get(a); // gets random dir
+        int CDir = dir.get(b);
+        int x = new Random().nextInt(wordsToAdd.size());
+        String RWord = wordsToAdd.get(x); // gets random word
 
+        int o = new Random.nextInt(data.length); // gets random start position
+        int p = new Random.nextInt(data[0].length);
+
+        if (addWord(RWord,o,p,RDir,CDir)) {
+          wordsToAdd.remove(RWord);
+          wordsAdded.add(RWord);
+          ++Tries;
+        } else {
+          o = new Random.nextInt(data.length);
+          p = new Random.nextInt(data[0].length);
+          ++Tries;
+        }
+      }
     }
-
 
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right, must fit on the WordGrid, and must
@@ -146,7 +171,7 @@ public class WordSearch{
     *have a corresponding letter to match any letters that it overlaps.
     *
     *@param word is any text to be added to the word grid.
-    *@param row is the vertical locaiton of where you want the word to start.
+    *@param row is the vertical location of where you want the word to start.
     *@param col is the horizontal location of where you want the word to start.
     *@return true when the word is added successfully. When the word doesn't fit,
     *or there are overlapping letters that do not match, then false is returned.
@@ -176,7 +201,7 @@ public class WordSearch{
     *and must have a corresponding letter to match any letters that it overlaps.
     *
     *@param word is any text to be added to the word grid.
-    *@param row is the vertical locaiton of where you want the word to start.
+    *@param row is the vertical location of where you want the word to start.
     *@param col is the horizontal location of where you want the word to start.
     *@return true when the word is added successfully. When the word doesn't fit,
     *or there are overlapping letters that do not match, then false is returned.
